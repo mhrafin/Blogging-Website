a07c8a6c206560beb37f451ed79d6f0d6bb00b0b
raflinhom <110781737+mhrafin@users.noreply.github.com>
2024-11-28 03:43:04 +0600

    RESTful routing implemented.
    RESTful routing implemented to add, edit and delete blog posts.
    Added a requirements.txt.
    Implemented SQLite through SQLAlchemy for data management.

--- main.py
+++ main.py
@@ -1,71 +1,157 @@
-import os
-import smtplib
-from datetime import datetime
+from datetime import date
 
-import requests
-from dotenv import load_dotenv
-from flask import Flask, render_template, request
-
-load_dotenv()
-
-EMAIL = os.getenv("EMAIL")
-PASSWORD = os.getenv("GMAIL_APP_PASS")
-
-
-RES = requests.get("https://api.npoint.io/bd19e496786f829612b2")
-ALL_POSTS = RES.json()
-
-TODAY = str(datetime.now().date())
-YEAR = str(datetime.now().year)
+from flask import Flask, redirect, render_template, request, url_for
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor, CKEditorField
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from wtforms import StringField, SubmitField
+from wtforms.validators import URL, DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+Bootstrap5(app)
 
 
-def send_email(message):
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=EMAIL, password=PASSWORD)
-        connection.sendmail(
-            from_addr=EMAIL,
-            to_addrs=EMAIL,
-            msg=f"Subject:Contacting From Raf Blogs\n\n{message}",
-        )
+ckeditor = CKEditor(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+class NewBlogForm(FlaskForm):
+    title = StringField("name", validators=[DataRequired()])
+    subtitle = StringField("subtitle", validators=[DataRequired()])
+    body = CKEditorField("body", validators=[DataRequired()])
+    author = StringField("author", validators=[DataRequired()])
+    img_url = StringField("img_url", validators=[DataRequired()])
+    submit = SubmitField("Add")
 
 
 @app.route("/")
-def home():
-    return render_template("index.html", posts=ALL_POSTS, today=TODAY)
+def get_all_posts():
+    # Queries the database for all the posts. Converts the data to a python list.
+    posts = db.session.execute(db.select(BlogPost)).scalars().all()
+    return render_template("index.html", all_posts=posts)
 
 
+# a route to click on individual posts.
+@app.route("/show-post")
+def show_post():
+    post_id = request.args.get("post_id")
+
+    # Retrieves a BlogPost from the database based on the post_id
+    requested_post = db.session.execute(
+        db.select(BlogPost).where(BlogPost.id == post_id)
+    ).scalar()
+    return render_template("post.html", post=requested_post)
+
+
+# create a new blog post
+@app.route("/add-new-post", methods=["GET", "POST"])
+def add_new_post():
+    form = NewBlogForm()
+    if request.method == "POST":
+        new_post = BlogPost(
+            title=request.form.get("title"),
+            subtitle=request.form.get("subtitle"),
+            date=date.today().strftime("%B %d, %Y"),
+            body=request.form.get("body"),
+            author=request.form.get("author"),
+            img_url=request.form.get("img_url"),
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form, h1="Add New Post")
+
+
+# TODO: edit_post() to change an existing blog post
+@app.route("/edit-post", methods=["GET", "POST"])
+def edit_post():
+    post_id = request.args.get("post_id")
+    post_date = (
+        db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id))
+        .scalar()
+        .date
+    )
+
+    selected_post = db.get_or_404(BlogPost, post_id)
+
+    form = NewBlogForm(
+        title=selected_post.title,
+        subtitle=selected_post.subtitle,
+        body=selected_post.body,
+        author=selected_post.author,
+        img_url=selected_post.img_url,
+    )
+    form.submit.label.text = "Update"
+
+    if request.method == "POST":
+        db.session.delete(selected_post)
+        db.session.commit()
+
+        new_post = BlogPost(
+            id=post_id,
+            title=request.form.get("title"),
+            subtitle=request.form.get("subtitle"),
+            date=post_date,
+            body=request.form.get("body"),
+            author=request.form.get("author"),
+            img_url=request.form.get("img_url"),
+        )
+
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post_id))
+    return render_template("make-post.html", form=form, h1="Edit Post")
+
+
+# TODO: delete_post() to remove a blog post from the database
+@app.route("/delete", methods=["Get", "POST"])
+def delete_post():
+    post_id = request.args.get("post_id")
+    selected_post = db.get_or_404(BlogPost, post_id)
+
+    db.session.delete(selected_post)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
+
+
+# Below is the code from previous lessons. No changes needed.
 @app.route("/about")
-def about_page():
+def about():
     return render_template("about.html")
 
 
-@app.route("/contact", methods=["GET", "POST"])
-def contact_page():
-    if request.method == "POST":
-        data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
-        message = f"Name: {data['name']}\nEmail: {data['email']}\nPhone: {data['phone']}\nMessage: {data['message']}"
-        send_email(message=message)
-        return render_template("contact_simple.html", msg="Success")
-
-    return render_template("contact_simple.html")
-
-
-@app.route("/post/<int:id>")
-def post_page(id):
-    clicked_post = None
-    for post in ALL_POSTS:
-        if post["id"] == id:  # Assuming each post has an 'id' field
-            clicked_post = post
-            break
-    return render_template("post.html", post=clicked_post, today=TODAY)
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5003)
