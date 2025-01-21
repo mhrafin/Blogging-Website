c529276d6929d6fe8acf0405040c9e8e46577476
raflinhom <110781737+mhrafin@users.noreply.github.com>
2025-01-22 01:22:02 +0600

    Rendering individual posts

--- main.py
+++ main.py
@@ -25,6 +25,16 @@ def about():
 def contact():
     return render_template("contact.html", current_year=current_year)
 
+@app.route("/post/<int:id>")
+def get_post(id):
+    res = req.get(data_url)
+    data = res.json()
+    the_post = None
+    for post in data:
+        if post["id"] == id:
+            the_post = post
+    return render_template("post.html", current_year=current_year, post=the_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
