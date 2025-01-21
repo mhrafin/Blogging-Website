54904fc03e2cc131e0d950989e3e64494304d98c
raflinhom <110781737+mhrafin@users.noreply.github.com>
2025-01-21 20:48:36 +0600

    Add page for displaying a single blog post.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import date
+import requests as req
 
 current_year = date.today().year
 
@@ -9,6 +10,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html", current_year=current_year)
+
+
+@app.route("/blog")
+def get_blog():
+    data_url = "https://api.npoint.io/c790b4d5cab58020d391"
+
+    res = req.get(data_url)
+
+    data = res.json()
+
+    return render_template("blog.html", posts=data)
+
 
 
 if __name__ == "__main__":
