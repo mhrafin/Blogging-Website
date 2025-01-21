e743f51f51b1801c7ef9bd5fee5ac9e74ed74371
raflinhom <110781737+mhrafin@users.noreply.github.com>
2025-01-22 00:54:02 +0600

    Fetch and render blog posts from an npoint API

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ from datetime import date
 import requests as req
 
 current_year = date.today().year
+data_url = "https://api.npoint.io/ee84059f6d2a9704021f"
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", current_year=current_year)
-
-
-@app.route("/blog/<int:id>")
-def get_blog(id):
-    data_url = "https://api.npoint.io/c790b4d5cab58020d391"
-
     res = req.get(data_url)
-
     data = res.json()
 
-    return render_template("blog.html", posts=data, id=id)
+    return render_template("index.html", current_year=current_year, posts=data)
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html", current_year=current_year)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", current_year=current_year)
 
 
 if __name__ == "__main__":
