b84ef7c81fe3673fed1f8f1d9ac3e61fb67de947
raflinhom <110781737+mhrafin@users.noreply.github.com>
2025-01-22 20:50:39 +0600

    Send email with smtplib

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
 from datetime import date
+
 import requests as req
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, url_for
+
+import email_sender
+
+load_dotenv()
+
+my_email = os.getenv("MY_EMAIL")
+my_password = os.getenv("MY_PASSWORD")
 
 current_year = date.today().year
 data_url = "https://api.npoint.io/ee84059f6d2a9704021f"
 
 app = Flask(__name__)
+
+
+def send_msg(name: str, email: str, phone: str, msg: str):
+    sender = email_sender.EmailSender(
+        smtp_host="smtp.gmail.com", sender_email=my_email, sender_password=my_password
+    )
+    sender.send_email(
+        recipient_email=my_email,
+        subject=f"Message from {name}!",
+        email_body=f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {msg}",
+    )
 
 
 @app.route("/")
@@ -29,6 +50,7 @@ def contact():
         phone = request.form.get("phone")
         message = request.form.get("message")
         print(name, email, phone, message)
+        send_msg(name=name, email=email, phone=phone, msg=message)
         return render_template("contact.html", current_year=current_year, msg_sent=True)
     return render_template("contact.html", current_year=current_year, msg_sent=False)
 
